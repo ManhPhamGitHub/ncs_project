d1c46f731544b6fcf4e561048ad18df124e39082
manh.pham <manh.pham@savvycomsoftware.com>
2024-12-19 23:52:40 +0700

    update nginx config

--- health_declaration_backend/src/main.ts
+++ health_declaration_backend/src/main.ts
@@ -5,6 +5,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }));
+  app.setGlobalPrefix("/api/");
+
   await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
